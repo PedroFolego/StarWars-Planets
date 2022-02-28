3c2568620b72033739c262ebfc919c3a6c884676
PedroFolego <hmfpedro@hotmail.com>
2022-02-28 12:02:04 -0300

    add ccs final

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -6,20 +6,24 @@ function Filters() {
   const { filterByNumericValues, removeFilter, removeAllFilter } = useContext(Context);
 
   return (
-    <nav className="filters">
-      {filterByNumericValues.map((filter) => (
-        <div data-testid="filter" key={ filter.column } className="filter">
-          <p className="name-filter">{filter.column}</p>
-          <button
-            type="button"
-            onClick={ () => removeFilter(filter.column) }
-          >
-            X
-          </button>
-        </div>
-      ))}
+    <nav className="nav__filters">
+      <div className="filters">
+        {filterByNumericValues.map((filter) => (
+          <div data-testid="filter" key={ filter.column } className="filter">
+            <p className="name-filter">{filter.column}</p>
+            <button
+              className="btn btn-danger btn-removeFilter"
+              type="button"
+              onClick={ () => removeFilter(filter.column) }
+            >
+              X
+            </button>
+          </div>
+        ))}
+      </div>
       {filterByNumericValues.length > 1 && (
         <button
+          className="btn btn-danger btn-deleteAll"
           type="button"
           data-testid="button-remove-filters"
           onClick={ removeAllFilter }
